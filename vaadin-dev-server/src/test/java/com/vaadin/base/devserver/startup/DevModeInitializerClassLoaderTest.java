171fa8cd5d0d61a7ad27028b69513c2a2c6dc861
Johannes Eriksson <joheriks@vaadin.com>
2021-05-11 00:12:54 +0300

    Dump. Extracting DevModeHandler and BrowserLiveReload into a separate module.

--- vaadin-dev-server/src/test/java/com/vaadin/base/devserver/startup/DevModeInitializerClassLoaderTest.java
+++ vaadin-dev-server/src/test/java/com/vaadin/base/devserver/startup/DevModeInitializerClassLoaderTest.java
@@ -1,4 +1,4 @@
-package com.vaadin.flow.server.startup;
+package com.vaadin.base.devserver.startup;
 
 import java.io.File;
 import java.lang.reflect.Field;
@@ -11,7 +11,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.vaadin.flow.server.frontend.TestUtils;
+import com.vaadin.flow.testutil.TestUtils;
 
 import static com.vaadin.flow.server.Constants.TARGET;
 import static com.vaadin.flow.server.frontend.FrontendUtils.DEFAULT_FLOW_RESOURCES_FOLDER;
